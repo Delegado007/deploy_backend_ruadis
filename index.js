2c488c8800b082b4b3dbacba529481684c96a65d
Delegado <maxharper2013@gmail.com>
2023-01-06 05:06:15 -0300

    [ADD] new cors

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const routerApi = require('./routes');
 const cors = require('cors');
 const { checkApiKey } = require('./middlewares/authHandler')
 const {
-  logErrors,
-  errorHandler,
-  boomErrorHandler,
-  ormErrorHandler,
+    logErrors,
+    errorHandler,
+    boomErrorHandler,
+    ormErrorHandler,
 } = require('./middlewares/errorHandler');
 
 const app = express();
@@ -15,30 +15,30 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 const whitelist = [
-  'http://localhost:3000',
-  'http://127.0.0.1:5600',
-  'http://localhost:3006',
-  'https://stellular-unicorn-408b27.netlify.app'
+    'http://localhost:3000',
+    'http://127.0.0.1:5600',
+    'http://localhost:3006',
+    'https://ruadis-v2.netlify.app/'
 ];
 const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('No permitido'));
-    }
-  },
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido'));
+        }
+    },
 };
 app.use(cors(options)); // sin el filtrado de option le estariamos dando acceso a todos los origenes
 
 require('./utils/auth')
 
 app.get('/', (req, res) => {
-  res.send('Hola mi server en express');
+    res.send('Hola mi server en express');
 });
 
 app.get('/nueva-ruta', checkApiKey, (req, res) => {
-  res.send('Hola soy una nueva ruta');
+    res.send('Hola soy una nueva ruta');
 });
 
 routerApi(app);
@@ -49,5 +49,5 @@ app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log('mi port' + port);
+    console.log('mi port' + port);
 });
